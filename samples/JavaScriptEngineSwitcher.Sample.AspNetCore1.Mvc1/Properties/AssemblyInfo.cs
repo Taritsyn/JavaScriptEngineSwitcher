33c237844e55faf12ed2c4d84b360c54ddbcf4f4
Andrey Taritsyn <taritsyn@gmail.com>
2016-08-22 17:11:18 +0300

    Added a sample ASP.NET Core 1.0 sites

---
+++ samples/JavaScriptEngineSwitcher.Sample.AspNetCore1.Mvc1/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JavaScriptEngineSwitcher.Sample.AspNetCore1.Mvc1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JavaScript Engine Switcher for .Net: Sample ASP.NET Core 1.0 MVC 1 Site")]
+[assembly: AssemblyCopyright("Copyright © 2013-2016 Andrey Taritsyn")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("061add3e-535f-4d6d-8b6c-22a01bddc485")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ samples/JavaScriptEngineSwitcher.Sample.AspNetCore1Full.Mvc1/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JavaScriptEngineSwitcher.Sample.AspNetCore1Full.Mvc1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JavaScript Engine Switcher for .Net: Sample ASP.NET Core 1.0 Full MVC 1 Site")]
+[assembly: AssemblyCopyright("Copyright © 2013-2016 Andrey Taritsyn")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6a7e3f16-3c75-425a-b15d-7b8db5b884c8")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
