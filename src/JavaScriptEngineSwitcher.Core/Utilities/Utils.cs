d89ccc0e5aa3ec7885d39c41f65f09231f4c4225
Andrey Taritsyn <taritsyn@gmail.com>
2017-11-06 20:49:49 +0300

    Now `.resx` files are converted by using the ResxToCs library

--- src/JavaScriptEngineSwitcher.Core/Utilities/Utils.cs
+++ src/JavaScriptEngineSwitcher.Core/Utilities/Utils.cs
@@ -5,7 +5,7 @@ using System.Linq;
 #endif
 using System.Reflection;
 using System.Runtime.CompilerServices;
-#if NETSTANDARD1_3
+#if NETSTANDARD1_3 || NETSTANDARD2_0
 using System.Runtime.InteropServices;
 #endif
 using System.Text;
@@ -42,7 +42,7 @@ namespace JavaScriptEngineSwitcher.Core.Utilities
 
 		private static bool InnerIsWindows()
 		{
-#if NETSTANDARD1_3
+#if NETSTANDARD1_3 || NETSTANDARD2_0
 			bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 #else
 			PlatformID[] windowsPlatformIDs =
