ed63109006adfdaa6d102292aa1f6ee82e80116e
Tomáš Deml <t.deml@quadient.com>
2019-02-12 09:49:18 +0100

    (Failing) Test to verify that embedded delegates are linked to the `Function` prototype

--- test/JavaScriptEngineSwitcher.Tests/ChakraCore/InteropTests.cs
+++ test/JavaScriptEngineSwitcher.Tests/ChakraCore/InteropTests.cs
@@ -1,10 +1,34 @@
-﻿namespace JavaScriptEngineSwitcher.Tests.ChakraCore
+﻿using System;
+using Xunit;
+
+namespace JavaScriptEngineSwitcher.Tests.ChakraCore
 {
 	public class InteropTests : InteropTestsBase
 	{
 		protected override string EngineName
 		{
 			get { return "ChakraCoreJsEngine"; }
+		}
+
+		[Fact]
+		public void EmbeddedInstanceOfDelegateHasFunctionPrototype()
+		{
+			// Arrange
+			var someFunc = new Func<int>(() => 42);
+
+			const string input = "Object.getPrototypeOf(embeddedFunc) === Function.prototype";
+
+			// Act
+			bool output;
+
+			using (var jsEngine = CreateJsEngine())
+			{
+				jsEngine.EmbedHostObject("embeddedFunc", someFunc);
+				output = jsEngine.Evaluate<bool>(input);
+			}
+
+			// Assert
+			Assert.True(output);
 		}
 	}
 }
