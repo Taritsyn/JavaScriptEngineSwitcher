e47d9940890018a43579c13d36a26695d49ed5f4
Taritsyn <taritsyn@gmail.com>
2019-12-22 11:40:14 +0300

    In sample projects was made refactoring of the `gulpfile.js` files

--- samples/JavaScriptEngineSwitcher.Sample.AspNet4.Mvc4/gulpfile.js
+++ samples/JavaScriptEngineSwitcher.Sample.AspNet4.Mvc4/gulpfile.js
@@ -2,46 +2,46 @@
 /*jshint esversion: 6 */
 
 // include plug-ins
-const { src, dest, series, parallel, watch } = require('gulp');
-const del = require('del');
-const sourcemaps = require('gulp-sourcemaps');
-const rename = require('gulp-rename');
-const concat = require('gulp-concat');
-const less = require('gulp-less');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCss = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
+let { src, dest, series, parallel, watch } = require('gulp');
+let del = require('del');
+let sourcemaps = require('gulp-sourcemaps');
+let rename = require('gulp-rename');
+let concat = require('gulp-concat');
+let less = require('gulp-less');
+let autoprefixer = require('gulp-autoprefixer');
+let cleanCss = require('gulp-clean-css');
+let uglify = require('gulp-uglify');
 
-const webRootPath = ".";
-const bowerDirPath = "lib";
-const styleDirPath = webRootPath + '/styles';
-const scriptDirPath = webRootPath + '/scripts';
+const WEB_ROOT_PATH = ".";
+const BOWER_DIR_PATH = "lib";
+const STYLE_DIR_PATH = WEB_ROOT_PATH + '/styles';
+const SCRIPT_DIR_PATH = WEB_ROOT_PATH + '/scripts';
 
 //#region Clean
 //#region Clean builded assets
 function cleanBuildedStyles() {
-	return del([styleDirPath + '/build/*']);
+	return del([STYLE_DIR_PATH + '/build/*']);
 }
 
 function cleanBuildedScripts() {
-	return del([scriptDirPath + '/build/*']);
+	return del([SCRIPT_DIR_PATH + '/build/*']);
 }
 
-const cleanBuildedAssets = parallel(cleanBuildedStyles, cleanBuildedScripts);
+let cleanBuildedAssets = parallel(cleanBuildedStyles, cleanBuildedScripts);
 //#endregion
 //#endregion
 
 //#region Build assets
 //#region Build styles
-const autoprefixerOptions = {
+let autoprefixerOptions = {
 	overrideBrowserslist: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1'],
 	cascade: true
 };
-const cssCleanOptions = { specialComments: '*' };
-const cssRenameOptions = { extname: '.min.css' };
+let cssCleanOptions = { specialComments: '*' };
+let cssRenameOptions = { extname: '.min.css' };
 
 function buildCommonStyles() {
-	return src([styleDirPath + '/app.less'])
+	return src([STYLE_DIR_PATH + '/app.less'])
 		.pipe(sourcemaps.init())
 		.pipe(less({
 			relativeUrls: true,
@@ -49,79 +49,79 @@ function buildCommonStyles() {
 		}))
 		.pipe(autoprefixer(autoprefixerOptions))
 		.pipe(sourcemaps.write('./'))
-		.pipe(dest(styleDirPath + '/build'))
+		.pipe(dest(STYLE_DIR_PATH + '/build'))
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(concat('common-styles.css'))
 		.pipe(cleanCss(cssCleanOptions))
 		.pipe(rename(cssRenameOptions))
 		.pipe(sourcemaps.write('./'))
-		.pipe(dest(styleDirPath + '/build'))
+		.pipe(dest(STYLE_DIR_PATH + '/build'))
 		;
 }
 
-const buildStyles = buildCommonStyles;
+let buildStyles = buildCommonStyles;
 //#endregion
 
 //#region Build scripts
-const jsConcatOptions = { newLine: ';' };
-const jsUglifyOptions = {
+let jsConcatOptions = { newLine: ';' };
+let jsUglifyOptions = {
 	output: { comments: /^!/ }
 };
-const jsRenameOptions = { extname: '.min.js' };
+let jsRenameOptions = { extname: '.min.js' };
 
 function buildModernizrScripts() {
-	return src([bowerDirPath + '/modernizr/modernizr.js'])
+	return src([BOWER_DIR_PATH + '/modernizr/modernizr.js'])
 		.pipe(sourcemaps.init())
 		.pipe(uglify(jsUglifyOptions))
 		.pipe(rename(jsRenameOptions))
 		.pipe(sourcemaps.write('./'))
-		.pipe(dest(scriptDirPath + '/build'))
+		.pipe(dest(SCRIPT_DIR_PATH + '/build'))
 		;
 }
 
 function buildCommonScripts() {
-	return src([scriptDirPath + '/common.js'])
+	return src([SCRIPT_DIR_PATH + '/common.js'])
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(rename({ basename: 'common-scripts' }))
 		.pipe(uglify(jsUglifyOptions))
 		.pipe(rename(jsRenameOptions))
 		.pipe(sourcemaps.write('./'))
-		.pipe(dest(scriptDirPath + '/build'))
+		.pipe(dest(SCRIPT_DIR_PATH + '/build'))
 		;
 }
 
 function buildEvaluationFormScripts() {
-	return src([bowerDirPath + '/jquery-validation/dist/jquery.validate.js',
-			bowerDirPath + '/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js',
-			bowerDirPath + '/bootstrap/js/button.js',
-			scriptDirPath + '/evaluation-form.js'])
+	return src([BOWER_DIR_PATH + '/jquery-validation/dist/jquery.validate.js',
+			BOWER_DIR_PATH + '/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js',
+			BOWER_DIR_PATH + '/bootstrap/js/button.js',
+			SCRIPT_DIR_PATH + '/evaluation-form.js'])
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(concat('evaluation-form-scripts.js', jsConcatOptions))
 		.pipe(uglify(jsUglifyOptions))
 		.pipe(rename(jsRenameOptions))
 		.pipe(sourcemaps.write('./'))
-		.pipe(dest(scriptDirPath + '/build'))
+		.pipe(dest(SCRIPT_DIR_PATH + '/build'))
 		;
 }
 
-const buildScripts = parallel(buildModernizrScripts, buildCommonScripts, buildEvaluationFormScripts);
+let buildScripts = parallel(buildModernizrScripts, buildCommonScripts, buildEvaluationFormScripts);
 //#endregion
 
-const buildAssets = parallel(buildStyles, buildScripts);
+let buildAssets = parallel(buildStyles, buildScripts);
 //#endregion
 
 //#region Watch assets
 function watchStyles() {
-	return watch([styleDirPath + '/**/*.{less,css}', '!' + styleDirPath + '/build/**/*.*'],
+	return watch([STYLE_DIR_PATH + '/**/*.{less,css}', '!' + STYLE_DIR_PATH + '/build/**/*.*'],
 		buildStyles);
 }
 
 function watchScripts() {
-	return watch([scriptDirPath + '/**/*.js', '!' + scriptDirPath + '/build/**/*.*'],
+	return watch([SCRIPT_DIR_PATH + '/**/*.js', '!' + SCRIPT_DIR_PATH + '/build/**/*.*'],
 		buildScripts);
 }
 
-const watchAssets = parallel(watchStyles, watchScripts);
+let watchAssets = parallel(watchStyles, watchScripts);
 //#endregion
 
 // Export tasks
