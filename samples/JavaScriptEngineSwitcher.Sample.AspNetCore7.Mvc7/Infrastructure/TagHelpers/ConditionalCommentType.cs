9dab9f46a3420a9120906bdb876a96431b03e680
Taritsyn <taritsyn@gmail.com>
2022-11-10 13:56:36 +0300

    In test and sample projects added support of .NET 7

---
+++ samples/JavaScriptEngineSwitcher.Sample.AspNetCore7.Mvc7/Infrastructure/TagHelpers/ConditionalCommentType.cs
@@ -0,0 +1,10 @@
+﻿namespace JavaScriptEngineSwitcher.Sample.AspNetCore7.Mvc7.Infrastructure.TagHelpers
+{
+	public enum ConditionalCommentType
+	{
+		Hidden,
+		Revealed,
+		RevealedValidating,
+		RevealedValidatingSimplified
+	}
+}
---
+++ samples/JavaScriptEngineSwitcher.Sample.AspNetCore7.Mvc7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JavaScriptEngineSwitcher.Sample.AspNetCore7.Mvc7.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
