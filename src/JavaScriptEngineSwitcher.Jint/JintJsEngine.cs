d69cd5a17e7d4d3e1413c407544f082ed73ad59d
Taritsyn <taritsyn@gmail.com>
2023-08-30 19:30:01 +0300

    In JavaScriptEngineSwitcher.Jint added support for the Jint version 3.0.0 Beta 2051

--- src/JavaScriptEngineSwitcher.Jint/JintJsEngine.cs
+++ src/JavaScriptEngineSwitcher.Jint/JintJsEngine.cs
@@ -51,7 +51,7 @@ namespace JavaScriptEngineSwitcher.Jint
 		/// <summary>
 		/// Version of original JS engine
 		/// </summary>
-		private const string EngineVersion = "3.0.0 Beta 2050";
+		private const string EngineVersion = "3.0.0 Beta 2051";
 
 		/// <summary>
 		/// Jint JS engine
