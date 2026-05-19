a5b87d8b71dad0355070d56395ec96a351928dd9
Taritsyn <taritsyn@gmail.com>
2026-05-19 13:05:13 +0300

    In JavaScriptEngineSwitcher.Yantra added support for the YantraJS version 1.2.368

--- src/JavaScriptEngineSwitcher.Yantra/YantraJsEngine.cs
+++ src/JavaScriptEngineSwitcher.Yantra/YantraJsEngine.cs
@@ -54,7 +54,7 @@ namespace JavaScriptEngineSwitcher.Yantra
 		/// <summary>
 		/// Version of original JS engine
 		/// </summary>
-		private const string EngineVersion = "1.2.367";
+		private const string EngineVersion = "1.2.368";
 
 		/// <summary>
 		/// Regular expression for working with the error message
