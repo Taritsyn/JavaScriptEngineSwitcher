00036cadf82ad4bfc4643ca01f761647f109d929
Taritsyn <taritsyn@gmail.com>
2026-05-19 11:42:57 +0300

    In JavaScriptEngineSwitcher.Jint added support for the Jint version 4.9.2

--- src/JavaScriptEngineSwitcher.Jint/JintJsEngine.cs
+++ src/JavaScriptEngineSwitcher.Jint/JintJsEngine.cs
@@ -59,7 +59,7 @@ namespace JavaScriptEngineSwitcher.Jint
 		/// <summary>
 		/// Version of original JS engine
 		/// </summary>
-		private const string EngineVersion = "4.9.1";
+		private const string EngineVersion = "4.9.2";
 
 		/// <summary>
 		/// Jint JS engine
